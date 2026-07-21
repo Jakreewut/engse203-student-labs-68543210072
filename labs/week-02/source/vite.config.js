2cf9a59699f71708078e1f9ae55c73e2c989ecee
Jakreewut <nongnack2504@gmail.com>
2026-07-21 21:11:47 +0700

    fix: build assets and update docs path

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 
-// TODO: เปลี่ยนให้ตรงชื่อ repository ของตนเอง เช่น engse203-lab02-67123456789
-const repositoryName = "engse203-student-labs-68543210072-3";
+const repositoryName = "engse203-student-labs-68543210072";
 
 export default defineConfig({
-  base: `/${repositoryName}/labs/week-02/`,
+  base: './', // 👈 แก้บรรทัดนี้เป็น './' ได้เลยครับ
   build: {
     outDir: "docs",
     emptyOutDir: true,
